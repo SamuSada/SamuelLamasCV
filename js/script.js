1863da2586d9f03a55b4977ba56617291aa063ac
Samuel Lamas <samusada95@gmail.com>
2024-04-08 21:52:38 +0200

    Versión 1.1 - Código optimizado y revisado

--- js/script.js
+++ js/script.js
@@ -1,62 +1,95 @@
-// script.js
-
 // Función para cargar el archivo JSON
 function cargarDatos() {
-    fetch('data/datos.json') // Aquí se ajusta la ruta del archivo JSON
-        .then(response => response.json())
-        .then(data => mostrarDatos(data))
-        .catch(error => console.error('Error al cargar los datos:', error));
+  fetch('data/datos.json')
+      .then(response => response.json())
+      .then(data => mostrarDatos(data))
+      .catch(error => handleError('Error al cargar los datos:', error));
 }
 
-// Función para mostrar los datos en el currículum
+// Función para mostrar datos
 function mostrarDatos(data) {
-    // Datos personales
-    document.getElementById('nombre').innerText = 'Nombre: ' + data.datosPersonales.nombre;
-    document.getElementById('correoElectronico').innerText = 'Correo electrónico: ' + data.datosPersonales.correoElectronico;
-    document.getElementById('telefono').innerText = 'Teléfono: ' + data.datosPersonales.telefono;
-    // document.getElementById('direccion').innerText = 'Dirección: ' + data.datosPersonales.direccion;
-
-    // Sobre Mi
-        // Datos personales
-        document.getElementById('sobreMi').innerText = data.sobreMi.descripcion;
-
-    // Estudios
-    const listaEstudios = document.getElementById('listaEstudios');
-    data.estudios.forEach(estudio => {
-        const item = document.createElement('li');
-        item.innerText = `${estudio.nivelTitulo} - ${estudio.titulo} - ${estudio.institucion} (${estudio.fechaFin})`;
-        listaEstudios.appendChild(item);
-    });
-
-    // Experiencia laboral
-    const listaExperiencia = document.getElementById('listaExperiencia');
-    data.experienciaLaboral.forEach(experiencia => {
-        const item = document.createElement('li');
-        item.innerHTML = `<strong>${experiencia.puesto}</strong> - ${experiencia.empresa} (${experiencia.fechaFin})<ul>${experiencia.responsabilidades.map(responsabilidad => `<li>${responsabilidad}</li>`).join('')}</ul>`;
-        listaExperiencia.appendChild(item);
-    });
+  mostrarDatosPersonales(data.datosPersonales);
+  mostrarSobreMi(data.sobreMi);
+  mostrarListaEstudios('listaEstudios', data.estudios, ['nivelTitulo', 'titulo', 'institucion', 'fechaFin']);
+  mostrarListaConocimientos('listaConocimientos', data.conocimientos, 'materia', 'nivel');
+  mostrarListaIdiomas('listaIdiomas', data.idiomas, 'idioma', 'nivel');
+  mostrarExperienciaLaboral(data.experienciaLaboral);
 }
 
-// Boton de Contacto
-let footerVisible = false;
+// Función Datos Personales
+function mostrarDatosPersonales(datosPersonales) {
+  document.getElementById('nombre').innerText = 'Nombre: ' + datosPersonales.nombre;
+  document.getElementById('correoElectronico').innerText = 'Correo electrónico: ' + datosPersonales.correoElectronico;
+  document.getElementById('telefono').innerText = 'Teléfono: ' + datosPersonales.telefono;
+}
 
+// Función para mostrar la sección "Sobre Mí"
+function mostrarSobreMi(sobreMi) {
+  document.getElementById('sobreMi').innerText = sobreMi.descripcion;
+}
+
+// Función Estudios
+function mostrarListaEstudios(idLista, data, propiedades) {
+  const lista = document.getElementById(idLista);
+  data.forEach(item => {
+      const elemento = document.createElement('li');
+      elemento.innerText = propiedades.map(propiedad => item[propiedad]).join(' - ');
+      lista.appendChild(elemento);
+  });
+}
+
+// Función Conocimientos
+function mostrarListaConocimientos(idLista, conocimientos) {
+  const lista = document.getElementById(idLista);
+  conocimientos.forEach(conocimiento => {
+      const elemento = document.createElement('li');
+      elemento.innerText = conocimiento.materia;
+      const medidor = document.createElement('div');
+      medidor.className = 'medidor';
+      const barra = document.createElement('div');
+      barra.className = 'barra';
+      barra.style.width = `${conocimiento.nivel}%`;
+      medidor.appendChild(barra);
+      elemento.appendChild(medidor);
+      lista.appendChild(elemento);
+  });
+}
+
+// Función Idiomas
+function mostrarListaIdiomas(idLista, idiomas) {
+  const lista = document.getElementById(idLista);
+  idiomas.forEach(idioma => {
+      const elemento = document.createElement('li');
+      elemento.innerText = idioma.idioma;
+      const medidor = document.createElement('div');
+      medidor.className = 'medidor';
+      const barra = document.createElement('div');
+      barra.className = 'barra';
+      barra.style.width = `${idioma.nivel}%`;
+      medidor.appendChild(barra);
+      elemento.appendChild(medidor);
+      lista.appendChild(elemento);
+  });
+}
+
+
+// Función Experiencia Laboral
+function mostrarExperienciaLaboral(experienciaLaboral) {
+  const lista = document.getElementById('listaExperiencia');
+  experienciaLaboral.forEach(experiencia => {
+      const elemento = document.createElement('li');
+      elemento.innerHTML = `<strong>${experiencia.puesto}</strong> - ${experiencia.empresa} (${experiencia.fechaFin})<ul>${experiencia.responsabilidades.map(responsabilidad => `<li>${responsabilidad}</li>`).join('')}</ul>`;
+      lista.appendChild(elemento);
+  });
+}
+
+// Mostrar/Ocultar Footer
 document.getElementById("toggleFooter").addEventListener("click", function() {
-  const footer = document.getElementById("footer");
-  
-  if (!footerVisible) {
-    footer.style.display = "flex";
-    document.body.style.overflow = "hidden";
-    setTimeout(() => footer.style.opacity = "1", 10);
-  } else {
-    footer.style.opacity = "0";
-    document.body.style.overflow = "auto";
-    setTimeout(() => footer.style.display = "none", 500);
-  }
-
-  footerVisible = !footerVisible;
+  var footer = document.getElementById("footer");
+  footer.classList.toggle("show");
 });
 
-// Llamar a la función para cargar los datos al cargar la página
+
+
+// Cargar datos al cargar la página
 window.onload = cargarDatos;
-
-
